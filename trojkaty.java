43a4552d1e89a4d751b69a431ba3704f31797524
Robert Jan Smoter <rsmoter@artemis.wszib.edu.pl>
2023-04-02 21:48:27 +0200

    Junior tester: pierwszy commit! :)

--- trojkaty.java
+++ trojkaty.java
@@ -4,9 +4,9 @@
 
 class Trojkaty {
     /**
-     * Główna fnukcja programu.
+     * Główna fukcja programu.
      * @param {float} a - Długość pierwszego boku.
-     * @param {float} b - Długość drugiegio boku.
+     * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
@@ -31,6 +31,11 @@ class Trojkaty {
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
         
-        jakiTrojkat(a, b, c);
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Długości boków trójkąta muszą być nieujemne!");
+            System.exit(2);
+           }
+
+
     }
 }
